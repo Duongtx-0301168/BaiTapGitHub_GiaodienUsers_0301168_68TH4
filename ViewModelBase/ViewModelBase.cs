438b34ac76794b7f1a91f5685f599550091b0a7a
duongtx_0301168 <xuanduongtrinh1007@gmail.com>
2026-01-29 22:55:21 +0700

    Thêm ViewModelBase

---
+++ ViewModelBase/ViewModelBase.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel;
+using System.Runtime.ComplierServices;
+
+namespace GiaodienUsers.ViewModels
+{
+    public class  ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null )
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null )
+        {
+            if (equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
